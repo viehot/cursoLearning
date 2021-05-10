200de3d82cee03335abe59b58e0f685128d3fc01
viejo <piri.viejo@gmail.com>
2021-05-09 23:26:53 -0300

    Add section school and apply DOM

--- unidad4/index.js
+++ unidad4/index.js
@@ -2,3 +2,17 @@ function oscuro() {
     var body = document.body
     body.classList.toggle("oscuro")
 }
+
+function mostrarSchool (bloque) {
+    var bloqueSchool = document.getElementsByClassName(bloque)
+    for(var i = 0; i<bloqueSchool.length; i++) {
+        bloqueSchool[i].style.display = 'block'
+    }
+}
+
+function ocultarSchool (bloque) {
+    var bloqueSchool = document.getElementsByClassName(bloque)
+    for(var i = 0; i<bloqueSchool.length; i++) {
+        bloqueSchool[i].style.display = 'none'
+    }
+}
